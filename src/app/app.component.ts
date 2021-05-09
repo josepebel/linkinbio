e57a780c1fbf0130fcae973503a00beb9c54b362
josepebel <josepe.beldad@gmail.com>
2021-05-09 19:48:08 +0200

    added credly-icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,8 +29,8 @@ export class AppComponent {
 
   card3: Card = {
     page: "Your Acclaim",
-    icon: "chevron-double-up",
-    color: "#26689a",
+    icon: "award",
+    color: "#00423b",
     url: "https://www.youracclaim.com/users/jose-antonio-peno-beldad/badges?sort=-state_updated_at&page=1"
   }
   card4: Card = {
